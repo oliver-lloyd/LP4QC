6b98d92ec18bbf73864379a0b665947d61ae0bfb
fu19841 <fu19841@bristol.ac.uk>
2022-10-02 16:11:52 +0100

    Writing results on the go

--- analysis/node2vec/assessment.py
+++ analysis/node2vec/assessment.py
@@ -5,25 +5,34 @@ from sklearn.linear_model import LinearRegression
 from os import listdir
 
 
-def assess_model_fit(embedding_file, embedding_path, edges_df):
-
-    # Load and prepare edge vectors
-    embeds = pd.read_csv(f'{embedding_path}/{embedding_file}', header=None, index_col=[0, 1])
-    embeds = embeds.loc[edges_df.index]  # Index of both is multi-index of dyads
-    embeds.dropna(how='all', axis=1, inplace=True)
+def assess_model_fit(embedding_file, embedding_path, edges_df, out_file):
 
     # Get meta info
     method = embedding_file.split('_')[0]
     p = float(embedding_file.split('_')[1][1:])
     q = float(embedding_file.split('_')[2][1:-12])
 
-    # Perform and assess regression
-    model = LinearRegression()
-    model.fit(embeds, edges_df.weight)
-    r2 = model.score(embeds, edges_df.weight)
+    # Check if results exists, run if not
+    results = pd.read_csv(out_file)
+    existing = [[row.method, row.p, row.q] for i, row in results.iterrows()]
 
-    # Return result and meta info
-    return [method, p, q, r2]
+    if [method, p, q] not in existing:
+        # Load and prepare edge vectors
+        embeds = pd.read_csv(f'{embedding_path}/{embedding_file}', header=None, index_col=[0, 1])
+        embeds = embeds.loc[edges_df.index]  # Index of both is multi-index of dyads
+        embeds.dropna(how='all', axis=1, inplace=True)
+
+        # Perform and assess regression
+        model = LinearRegression()
+        model.fit(embeds, edges_df.weight)
+        r2 = model.score(embeds, edges_df.weight)
+
+        # Write result and meta info
+        csv_string = ','.join([str(val) for val in [method, p, q, r2]])
+        with open(out_file, 'a') as f:
+            f.write(csv_string + '\n')
+    else:
+        print('Skip')
 
 
 if __name__ == '__main__':
@@ -43,16 +52,17 @@ if __name__ == '__main__':
         raise ValueError('Either "weight" column must exist in edgelist, or pass name of weight column with --weight_var')
     edgelist = edgelist[['weight']]
 
+    # Create results file if doesn't exists
+    result_file = 'model_fit_results.csv'
+    if result_file not in listdir():
+        pd.DataFrame(columns=['method', 'p', 'q', 'R2']).to_csv(result_file, index=False)
+
     # Set up parallel args
     para_args = [
-        [embed_file, args.edge_vec_dir, edgelist]
+        [embed_file, args.edge_vec_dir, edgelist, result_file]
         for embed_file in listdir(args.edge_vec_dir)
     ]
 
     # Assess regression fit of edge weight on edge vector 
     with mp.Pool(mp.cpu_count()) as pool:
         results = pool.starmap(assess_model_fit, para_args)
-
-    # Save results as CSV
-    out_df = pd.DataFrame(results, columns=['method', 'p', 'q', 'R2']).sort_values('R2', ascending=False)
-    out_df.to_csv('model_fit_results.csv', index=False)
